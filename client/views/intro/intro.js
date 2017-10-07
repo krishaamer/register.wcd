0ba7e558fd920b16b6e6e0bed44e117789be60c9
Kris Haamer <kris@mindworks.ee>
2017-10-07 15:21:32 +0300

    Start Twilio integration

--- client/views/intro/intro.js
+++ client/views/intro/intro.js
@@ -11,23 +11,26 @@ Template.intro.helpers({
 	intro () {
 
 	    return Intro.findOne();
-	}
+	},
+  helloName () {
+
+    Meteor.subscribe("current_user_data");
+
+    /*
+    analytics.identify({
+      email: Meteor.user().profile.email,
+      first_name: Meteor.user().profile.first_name,
+      last_name: Meteor.user().profile.last_name
+    });
+    */
+
+    let name = '';
+    if (Meteor.user().profile)
+      name = Meteor.user().profile.first_name;
+
+    return name;
+  },
 });
-
-Template.intro.helloName = function() {
-
-  Meteor.subscribe("current_user_data");
-
-  /*
-  analytics.identify({
-    email: Meteor.user().profile.email,
-    first_name: Meteor.user().profile.first_name,
-    last_name: Meteor.user().profile.last_name
-  });
-  */
-
-  return Meteor.user().profile.first_name;
-}
 
 Template.intro.events({
   "click #logout": function (err, tmpl) {
@@ -41,25 +44,57 @@ Template.intro.events({
       }
     })
   },
-  "click #login": function (err, tmpl) {
+  "click #login-fb": function (err, tmpl) {
     
     //analytics.track("Login Clicked");
     
     Meteor.loginWithFacebook({ 
       requestPermissions: ['email']},
-    function (err) {
-        if (err) {
+      function (err) {
+          if (err) {
 
-        /*	
-          analytics.track("Login failed", {
-            problem: err,
-            why: err.reason
-          });
-        */
-          
-        } else {
-          //analytics.track("Login success");
-        }
-    });
-  }
+          /*	
+            analytics.track("Login failed", {
+              problem: err,
+              why: err.reason
+            });
+          */
+            
+          } else {
+            //analytics.track("Login success");
+          }
+
+          FlowRouter.go("/register");
+      });
+  },
+  "click #login-email": function (err, tmpl) {
+    
+      //analytics.track("Login Clicked");    
+  },
+  "click #login-phone": function (err, tmpl) {
+    
+      var options = {phone:'+37253073123'};
+      options.password = 'VeryHardPassword';
+
+      Accounts.createUserWithPhone(options, function (){});
+      
+      // Debug: Verify the user phone isn't confirmed it.
+      console.log('Phone verification status is :', Accounts.isPhoneVerified());
+      var userPhone = '+37253073123';
+      // Request for sms phone verification -- please note before receiving SMS you should Follow the SMS Integration tutorial below
+      Accounts.requestPhoneVerification(userPhone, function(){});
+      //Debug:  Verify the user phone isn't confirmed it.
+      console.log('Phone verification status is :', Accounts.isPhoneVerified());
+
+      // After receiving SMS let user enter his code and verify account by sending it to the server
+      var verificationCode = 'CodeRecivedBySMS';
+      var newPassword = null;
+      // You can keep your old password by sending null in the password field
+      Accounts.verifyPhone(userPhone, verificationCode, function(){});
+      //Debug:  Verify the user phone is confirmed.
+      console.log('Phone verification status is :', Accounts.isPhoneVerified());
+
+    console.log("register with phone");
+    
+  },
 })
--- server/accounts.js
+++ server/accounts.js
@@ -6,31 +6,59 @@ Meteor.publish("access_token", function () {
 
 Accounts.onCreateUser(function (options, user) {
 
-    let accessToken = user.services.facebook.accessToken, result, profile;
-	result = Meteor.http.get("https://graph.facebook.com/me", {
-		params: {
-		  access_token: accessToken
+	console.log(user);
+
+	if (user.services.phone) {
+
+		let options = {phone:'+37253073123'};
+		options.password = 'VeryHardPassword';
+		Accounts.createUserWithPhone(options, function (){});
+		//console.log('Phone verification status is :', Accounts.isPhoneVerified());
+
+		SMS.twilio = {
+			FROM: '+37258821463', 
+			ACCOUNT_SID: 'ACb26de4522ecdd582d6bc603de0ad7348', 
+			AUTH_TOKEN: '352a128bc687411a3673e6074c56d947'
+		};
+
+		SMS.send = function (options) {};
+
+		SMS.phoneTemplates = {
+		    from: '+37258821463',
+		    text: function (user, code) {
+
+		        return 'Welcome your invitation code is: ' + code;
+		    }
+		 };
+	}
+
+	if (user.services.facebook) {
+	    let accessToken = user.services.facebook.accessToken, result, profile;
+		result = Meteor.http.get("https://graph.facebook.com/me", {
+			params: {
+			  access_token: accessToken
+			}
+		});
+
+		/*
+		if (result.error)
+			throw result.error;
+		*/
+
+		profile = _.pick(result.data, "services.facebook.name", "services.facebook.picture", "services.facebook.email");
+
+		// We still want the default hook's 'profile' behavior.
+		if (options.profile) {
+
+			options.profile.picture 	= "http://graph.facebook.com/" + user.services.facebook.id + "/picture/?type=large";
+			options.profile.first_name 	= user.services.facebook.first_name;
+			options.profile.last_name 	= user.services.facebook.last_name;
+			options.profile.gender 		= user.services.facebook.gender;
+			options.profile.email		= user.services.facebook.email;
+			options.profile.has_pledged = false;
+			options.profile.message		= "";
+			user.profile = options.profile;
 		}
-	});
-
-	/*
-	if (result.error)
-		throw result.error;
-	*/
-
-	profile = _.pick(result.data, "services.facebook.name", "services.facebook.picture", "services.facebook.email");
-
-	// We still want the default hook's 'profile' behavior.
-	if (options.profile) {
-
-		options.profile.picture 	= "http://graph.facebook.com/" + user.services.facebook.id + "/picture/?type=large";
-		options.profile.first_name 	= user.services.facebook.first_name;
-		options.profile.last_name 	= user.services.facebook.last_name;
-		options.profile.gender 		= user.services.facebook.gender;
-		options.profile.email		= user.services.facebook.email;
-		options.profile.has_pledged = false;
-		options.profile.message		= "";
-		user.profile = options.profile;
 	}
 	
     return user;
@@ -72,3 +100,20 @@ Meteor.publish("current_user_data", function () {
 	return Meteor.users.find({_id: this.userId},
 		{fields: {'profile.first_name': 1, 'profile.last_name': 1, 'profile.picture': 1}});
 });
+
+Accounts.emailTemplates.siteName = "World Cleanup Day";
+Accounts.emailTemplates.from = "World Cleanup Day <hi@worldcleanupday.com>";
+Accounts.emailTemplates.resetPassword = {
+  subject(user) {
+    return "Reset your password on World Cleanup Day";
+  },
+  text(user, url) {
+    return `Hello!
+		Click the link below to reset your password on Meteor Todos.
+		${url}
+		If you didn't request this email, please ignore it.
+		Thanks,
+		World Cleanup Day`
+  },
+  html(user, url) {}
+};
