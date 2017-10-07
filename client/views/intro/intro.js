5a48e468f5ab31ba52f88d00b5fc8522d4824673
Kris Haamer <kris@mindworks.ee>
2017-10-07 12:01:01 +0300

    v0.1

--- client/views/intro/intro.js
+++ client/views/intro/intro.js
@@ -13,3 +13,53 @@ Template.intro.helpers({
 	    return Intro.findOne();
 	}
 });
+
+Template.intro.helloName = function() {
+
+  Meteor.subscribe("current_user_data");
+
+  /*
+  analytics.identify({
+    email: Meteor.user().profile.email,
+    first_name: Meteor.user().profile.first_name,
+    last_name: Meteor.user().profile.last_name
+  });
+  */
+
+  return Meteor.user().profile.first_name;
+}
+
+Template.intro.events({
+  "click #logout": function (err, tmpl) {
+    Meteor.logout(function (err) {      
+      if (err) {
+        //Session.set("status", err.reason);
+        //analytics.track("Logout error");
+        
+      } else {
+        //analytics.track("Logout success");
+      }
+    })
+  },
+  "click #login": function (err, tmpl) {
+    
+    //analytics.track("Login Clicked");
+    
+    Meteor.loginWithFacebook({ 
+      requestPermissions: ['email']},
+    function (err) {
+        if (err) {
+
+        /*	
+          analytics.track("Login failed", {
+            problem: err,
+            why: err.reason
+          });
+        */
+          
+        } else {
+          //analytics.track("Login success");
+        }
+    });
+  }
+})
---
+++ server/accounts.js
@@ -0,0 +1,74 @@
+Meteor.publish("access_token", function () {
+	return Meteor.users.find({ _id : this.userId}, 
+		{'services.facebook.accessToken': 1}
+	);
+});
+
+Accounts.onCreateUser(function (options, user) {
+
+    let accessToken = user.services.facebook.accessToken, result, profile;
+	result = Meteor.http.get("https://graph.facebook.com/me", {
+		params: {
+		  access_token: accessToken
+		}
+	});
+
+	/*
+	if (result.error)
+		throw result.error;
+	*/
+
+	profile = _.pick(result.data, "services.facebook.name", "services.facebook.picture", "services.facebook.email");
+
+	// We still want the default hook's 'profile' behavior.
+	if (options.profile) {
+
+		options.profile.picture 	= "http://graph.facebook.com/" + user.services.facebook.id + "/picture/?type=large";
+		options.profile.first_name 	= user.services.facebook.first_name;
+		options.profile.last_name 	= user.services.facebook.last_name;
+		options.profile.gender 		= user.services.facebook.gender;
+		options.profile.email		= user.services.facebook.email;
+		options.profile.has_pledged = false;
+		options.profile.message		= "";
+		user.profile = options.profile;
+	}
+	
+    return user;
+});
+
+/* Make sure we're using the correct API keys for the dev / live servers */
+ServiceConfiguration.configurations.remove({
+  service: "facebook"
+});
+
+if(Meteor.absoluteUrl() == "http://localhost:3000/"){
+
+	ServiceConfiguration.configurations.upsert(
+	{ service: "facebook" },
+	  {
+	    $set: {
+	      appId: "1613725145554917",
+	      loginStyle: "popup",
+	      secret: "abf743e3c480aa4894cb474fdf2f3192"
+	    }
+	  }
+	);
+
+} else {
+
+	ServiceConfiguration.configurations.upsert(
+	{ service: "facebook" },
+	  {
+	    $set: {
+	      appId: "522400794771676",
+	      loginStyle: "popup",
+	      secret: "7fd62e55d35e7e5490355f3c14d7f71c"
+	    }
+	  }
+	);
+}
+
+Meteor.publish("current_user_data", function () {
+	return Meteor.users.find({_id: this.userId},
+		{fields: {'profile.first_name': 1, 'profile.last_name': 1, 'profile.picture': 1}});
+});
